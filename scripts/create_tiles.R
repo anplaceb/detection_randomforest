2cc0d1af52bed7a17d339f29f61cc7ebb9d41c46
anplaceb <anplaceb@gmail.com>
2023-10-05 09:25:48 +0200

    Change name

--- scripts/create_tiles.R
+++ scripts/create_tiles.R
@@ -1,9 +1,9 @@
 library(renv)
 library(terra)
 
-makeTiles(x=rast(r"{Y:\Andrea\wsf-sat\data\s2_mosaic_gee\s2_2023\ortho_gee_2023_11bands.tif}"), 
+makeTiles(x=rast(r"{Y:\Andrea\wsf-sat\data\s2_mosaic_gee\s2_2023\gee_2023_11bands.tif}"), 
           y=c(3000, 3000),
-          filename=r"{Y:\Andrea\wsf-sat\data\s2_mosaic_gee\s2_2023\tiles\ortho_gee_2023_.tif}", 
+          filename=r"{Y:\Andrea\wsf-sat\data\s2_mosaic_gee\s2_2023\tiles\gee_2023_.tif}", 
           na.rm=TRUE)
 
 
