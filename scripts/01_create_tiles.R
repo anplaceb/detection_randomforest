a303a7aad41e6ea07a7883714bb227ab84889de0
anplaceb <anplaceb@gmail.com>
2023-12-08 13:57:50 +0100

    comment

--- scripts/01_create_tiles.R
+++ scripts/01_create_tiles.R
@@ -5,7 +5,7 @@ library(terra)
 
 input_image_path <- r"{Y:\Andrea\wsf-sat\data\s2_mosaic_gee\s2_2020\gee_2020_11bands.tif}"
 output_image_path <- r"{Y:\Andrea\wsf-sat\data\s2_mosaic_gee\s2_2020\tiles\gee_2020_.tif}"
-size_tile <- c(3000, 3000)
+size_tile <- c(3000, 3000) # in meters
 
 makeTiles(x=rast(input_image_path), 
           y=size_tile,
