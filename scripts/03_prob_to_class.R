ee1f6f305faeab45983166d36c1071fa36c182cf
anplaceb <anplaceb@gmail.com>
2023-12-13 12:40:27 +0100

    Same

--- scripts/03_prob_to_class.R
+++ scripts/03_prob_to_class.R
@@ -5,9 +5,9 @@ library(renv)
 # Set input raster
 # Set outpt file name
 # Set threshold value to clamp input raster
-input_raster <- r"{D:\wsf-sat\methods\scripts\detection_randomforest\output\predictions\model_rf_lokal_10000_181023_3var_prob\2023\rf_prob_prediction_2023.tif}"
-output_file_name <- r"{D:\wsf-sat\methods\postprocessing\rf_postprocessing\model_rf_lokal_10000_181023_3var_prob\detection_input_for_postprocessing\2023.tif}"
-threshold_value <- 1
+input_raster <- r"{P:\WSF-SAT\Permanent\Methoden\Software\RandomForest\detection_randomforest\output\predictions\model_rf_lokal_10000_181023_3var_prob\2022\rf_prob_prediction_2022.tif}"
+output_file_name <- r"{P:\WSF-SAT\Permanent\Methoden\Software\RandomForest\detection_randomforest\output\predictions\model_rf_lokal_10000_181023_3var_prob\2022\rf_prob_prediction_2022_class.tif}"
+threshold_value <- 1 # probability of being no damage
 
 # Env options
 terraOptions(tempdir=r"{Y:\Andrea}")
@@ -15,10 +15,8 @@ terraOptions()
 wopt_options <- list(gdal = c("NUM_THREADS = ALL_CPUS"))
 
 # Load raster
-r <- rast(input_raster)+1
-
+r <- rast(input_raster)
 # Clamp values of the raster to a maximal value 
-# Multiply with -1 to convert to 1
 r_clamp <- clamp(r, lower=threshold_value, value=FALSE) 
 writeRaster(x=r_clamp, filename=output_file_name, 
             filetype="COG", datatype='INT4S', overwrite=TRUE, wopt=wopt_options)
